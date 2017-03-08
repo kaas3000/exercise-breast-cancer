6c3e3c9f08ab2e79a2a8905034a835e17a3830a6
Kees Meliefste <k_meliefste@hotmail.nl>
2017-03-08 17:05:30 +0100

    Implemented forward pass

---
+++ breast_cancer.py
@@ -0,0 +1,59 @@
+"""
+Train a neural network on breast cancer data
+"""
+
+import numpy as np
+from numpy import genfromtxt
+
+DATASET_LOCATION = 'res/breast-cancer-wisconsin.data'
+
+DATASET = genfromtxt(DATASET_LOCATION, delimiter=',')
+INPUT_DATA = np.delete(DATASET, [0, 10], axis=1)
+OUTPUT_DATA = DATASET[:, -1]
+
+# Global network properties
+LAYERS = [9, 9, 1]
+ALL_WEIGHTS = np.vstack((np.ones((1, 9))))
+ALL_WEIGHTS = np.vstack((ALL_WEIGHTS, np.random.rand(9, 9)))
+ALL_WEIGHTS = np.vstack((ALL_WEIGHTS, np.random.rand(9, 9)))
+
+
+def sigmoid_activation(val):
+    """
+    Sigmoid activation function. This function is used
+    to squash values between 0 and 1.
+    :param val: the variable used in the sigmoid
+    :return: a value between 0 and 1
+    """
+    return 1 / (1 + np.exp(-val))
+
+
+def forwardpass():
+    """
+    Forward pass
+    """
+
+    # Setup
+    current_layer = 0
+    current_input = INPUT_DATA[0]
+
+    # Forward pass: loop through network
+    for layer_size in LAYERS:
+        layer_weights = ALL_WEIGHTS[current_layer]
+
+        # multiply weights by input
+        layer_weights = np.tile(current_input, [layer_size, 1]) * layer_weights
+
+        new_input = []
+
+        for activation_input in [np.sum(neuron_input) for neuron_input in layer_weights]:
+            tatuu = sigmoid_activation(activation_input)
+            new_input.append(sigmoid_activation(tatuu))
+
+        current_input = new_input
+        current_layer += 1
+
+    print current_input
+
+
+forwardpass()
